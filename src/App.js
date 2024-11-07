3de6ce1cc271fa189fe5bdcca1d4860fa55d6e19
KazumaProject <59742125+KazumaProject@users.noreply.github.com>
2024-11-06 19:01:53 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      if (inputValue) {
+        fetchResults(inputValue);
+      } else {
+        setResults([]);
+      }
+    }, 500);
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [inputValue]);
+
+  const fetchResults = async (value) => {
+    const formattedValue = value.charAt(0).toUpperCase() + value.slice(1);
+
+    try {
+      const response = await axios.post('http://localhost:8080/henkan', {
+        value: formattedValue,
+      });
+      
+      if (response.data && Array.isArray(response.data.result)) {
+        setResults(response.data.result);
+      } else {
+        console.error('Unexpected response format:', response.data);
+        setResults([]);
+      }
+    } catch (error) {
+      console.error('Error fetching conversion:', error);
+      setResults([]);
+    }
+  };
+
+  const handleItemClick = (japanese) => {
+    // Open Instagram hashtag page with the response data
+    const hashtag = encodeURIComponent(japanese); // Encode hashtag for URL
+    window.open(`https://www.instagram.com/explore/tags/${hashtag}/`, '_blank');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> 奈良のラーメン屋</h1>
+      <div className="search-box">
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="Enter text in English"
+        />
+      </div>
+      <div className="results">
+        <h2>Conversion Results:</h2>
+        <ul>
+          {results.map(([english, japanese], index) => (
+            <li key={index} onClick={() => handleItemClick(japanese)}>
+              {english} - {japanese}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
